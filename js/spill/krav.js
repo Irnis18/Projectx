a0e2f922ab6ba6707e136f90b1109c20c4a0f1dd
bjornsiv <55831515+bjornsiv@users.noreply.github.com>
2019-10-09 20:27:11 +0200

    Fullført Krav siden
    Skrev ferdig krav.html
    opprettet krav.js
    Lagde en tabell med nettlesere og hvilken versjon som man må ha for å kjøre phaser.
    Lagde en knapp som sjekker hvilken nettleser man har og hvilken versjon man har. fungerer ikke for safari eller mobile enheter

---
+++ js/spill/krav.js
@@ -0,0 +1,27 @@
+$(document).ready(function (){
+  $("#btnChk").click(function (){
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+      $("#txtUt").text("Beklager, denne funksjonaliteten fungerer ikke på mobile enheter");
+    }
+    else if (/Edge/.test(navigator.userAgent)) {
+      console.log ("User has Edge installed.");
+      $("#txtUt").text("Du har Microsoft Edge, versjon " + navigator.userAgent.slice(121, 123));
+    }
+    else if(/Opera/.test(navigator.userAgent)|| /OPR/.test(navigator.userAgent)){
+      console.log ("User has Opera installed.");
+      $("#txtUt").text("Du har Opera, versjon " + navigator.userAgent.slice(119, 120));
+    }
+    else if(navigator.userAgent.indexOf("Firefox") != -1 ){
+      console.log ("User has Firefox installed.");
+      $("#txtUt").text("Du har Firefox, versjon " + navigator.userAgent.slice(74, 76));
+    }
+    else if(navigator.userAgent.indexOf("Chrome") != -1 ){
+      console.log ("User has Chrome installed.");
+      $("#txtUt").text("Du har Google Chrome, versjon " + navigator.userAgent.slice(88, 90));
+    }
+    else if(navigator.userAgent.indexOf("Safari") != -1){
+      console.log ("User has Safari installed.");
+      $("#txtUt").text("Du har Safari, versjonummeret kan ikke hentes");
+    }
+  });
+});
