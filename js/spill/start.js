c670af0e1fb6309c2214039a4c881a287200799f
irnis18 <irnis@omt.tech>
2019-10-15 17:10:01 +0200

    setup demo for game

--- js/spill/start.js
+++ js/spill/start.js
@@ -16,18 +16,24 @@ var config = {
   }
 };
 
+var player;
+var consolls;
+var bombs;
+var platforms;
+var cursors;
+var score = 0;
+var gameOver = false;
+var scoreText;
+
 let game = new Phaser.Game(config);
 
 function preload() {
   this.load.image('forest', '../../assets/img/spill/maps/map4.png');
-  this.load.image(
-    'ground',
-    '../../assets/img/spill/platform/platform-iland.png'
-  );
+  this.load.image('ground', '../../assets/img/spill/platform/platform.png');
   this.load.image('consoll', '../../assets/img/spill/consolle-small.png');
   this.load.image('bomb', '../../assets/img/spill/bomb.png');
-  this.load.spritesheet('dude', '../../assets/img/spill/dude.png', {
-    frameWidth: 32,
+  this.load.spritesheet('dude', '../../assets/img/spill/dude2.png', {
+    frameWidth: 31,
     frameHeight: 48
   });
 }
@@ -35,16 +41,130 @@ function preload() {
 function create() {
   this.add.image(400, 300, 'forest');
 
-  let platforms = this.physics.add.staticGroup();
+  platforms = this.physics.add.staticGroup();
 
   platforms
     .create(400, 568, 'ground')
-    .setScale(3)
+    .setScale(2)
     .refreshBody();
 
   platforms.create(600, 400, 'ground');
   platforms.create(50, 250, 'ground');
   platforms.create(750, 220, 'ground');
+  platforms.create(60, 420, 'ground');
+
+  player = this.physics.add.sprite(100, 450, 'dude');
+
+  player.setBounce(0.5);
+  player.setCollideWorldBounds(true);
+
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: 'turn',
+    frames: [{ key: 'dude', frame: 4 }],
+    frameRate: 20
+  });
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  cursors = this.input.keyboard.createCursorKeys();
+
+  consolls = this.physics.add.group({
+    key: 'consoll',
+    repeat: 11,
+    setXY: { x: 12, y: 0, stepX: 70 }
+  });
+
+  consolls.children.iterate(function(child) {
+    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+  });
+
+  bombs = this.physics.add.group();
+
+  scoreText = this.add.text(16, 16, 'score: 0', {
+    fontSize: '32px',
+    fill: '#000'
+  });
+
+  this.physics.add.collider(player, platforms);
+  this.physics.add.collider(consolls, platforms);
+  this.physics.add.collider(bombs, platforms);
+
+  this.physics.add.overlap(player, consolls, collectConsoll, null, this);
+
+  this.physics.add.collider(player, bombs, hitBomb, null, this);
 }
 
-function update() {}
+function update() {
+  if (gameOver) {
+    return;
+  }
+
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+
+    player.anims.play('left', true);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+
+    player.anims.play('right', true);
+  } else {
+    player.setVelocityX(0);
+
+    player.anims.play('turn');
+  }
+
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
+  }
+}
+
+function collectConsoll(player, consoll) {
+  consoll.disableBody(true, true);
+
+  //  Add and update the score
+  score += 10;
+  scoreText.setText('Score: ' + score);
+
+  if (consolls.countActive(true) === 0) {
+    //  A new batch of stars to collect
+    consolls.children.iterate(function(child) {
+      child.enableBody(true, child.x, 0, true, true);
+    });
+
+    var x =
+      player.x < 400
+        ? Phaser.Math.Between(400, 800)
+        : Phaser.Math.Between(0, 400);
+
+    var bomb = bombs.create(x, 16, 'bomb');
+    bomb.setBounce(1);
+    bomb.setCollideWorldBounds(true);
+    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    bomb.allowGravity = false;
+  }
+}
+
+function hitBomb(player, bomb) {
+  this.physics.pause();
+
+  player.setTint(0xff0000);
+
+  player.anims.play('turn');
+
+  gameOver = true;
+}
