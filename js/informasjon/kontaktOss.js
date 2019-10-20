38a02792984d39983b3db736ed18b46b5abc3df6
irnis18 <irnis@omt.tech>
2019-10-20 23:55:49 +0200

    fiksa alle informasjons sidene

--- js/informasjon/kontaktOss.js
+++ js/informasjon/kontaktOss.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
   $('#mailBtn').click(function() {
-      $('#mailForm').attr('action',
-                          'mailto:siljmd@stud.ntnu.no?subject=' +
-                          $('#mailEmne').val() + '&body=Avsenders E-post: ' + $('#mailAdr').val() + escape('\r\n') + 'Avsenders Navn: ' + $('#mailNavn').val() + escape('\r\n') + 'Avsenders Melding: ' + escape('\r\n') + $('#mailMld').val() + escape('\r\n'));
-      $('#mailForm').submit();
-    });
+    $('#mailForm').attr(
+      'action',
+      'mailto:siljmd@stud.ntnu.no?subject=' +
+        $('#mailEmne').val() +
+        '&body=Avsenders E-post: ' +
+        $('#mailAdr').val() +
+        escape('\r\n') +
+        'Avsenders Navn: ' +
+        $('#mailNavn').val() +
+        escape('\r\n') +
+        'Avsenders Melding: ' +
+        escape('\r\n') +
+        $('#mailMld').val() +
+        escape('\r\n')
+    );
+    $('#mailForm').submit();
+  });
 });
