0e9b23a9ce3da15187e986635112ed37c53d92a1
bjornsiv <55831515+bjornsiv@users.noreply.github.com>
2019-10-30 15:20:06 +0100

    Endret preloaderScene.js fra gameMapTwoScene til  Title

--- spill/src/scenes/preloaderScene.js
+++ spill/src/scenes/preloaderScene.js
@@ -93,10 +93,10 @@ export default class PreloaderScene extends Phaser.Scene {
   }
 
   ready() {
-    this.scene.start("GameMapTwo");
+    this.scene.start("Title");
     this.readyCount++;
     if (this.readyCount === 2) {
-      this.scene.start("GameMapTwo");
+      this.scene.start("Title");
     }
   }
 }
