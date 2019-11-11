5e10e2d944ce3522765183b49f3abe51785463c3
Sivert <sivertbjorn@gmail.com>
2019-11-11 12:13:49 +0100

    Update krav.js

--- nettside/js/krav.js
+++ nettside/js/krav.js
@@ -1,44 +1,48 @@
 //Function that waits untill all the html is loaded before excecuting
 $(document).ready(function() {
   //Function that checks what version and browser the user is at
-  $('#btnChk').click(function() {
+  $("#btnChk").click(function() {
     if (
       /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
         navigator.userAgent
       )
     ) {
-      $('#txtUt').text(
-        'Beklager, denne funksjonaliteten fungerer ikke på mobile enheter'
+      $("#txtUt").text(
+        "Beklager, denne funksjonaliteten fungerer ikke på mobile enheter"
       );
     } else if (/Edge/.test(navigator.userAgent)) {
-      console.log('User has Edge installed.');
-      $('#txtUt').text(
-        'Du har Microsoft Edge, versjon ' + navigator.userAgent.slice(121, 123)
+      console.log("User has Edge installed.");
+      $("#txtUt").text(
+        "Du har Microsoft Edge, versjon " + navigator.userAgent.slice(121, 123)
       );
     } else if (
       /Opera/.test(navigator.userAgent) ||
       /OPR/.test(navigator.userAgent)
     ) {
-      console.log('User has Opera installed.');
-      $('#txtUt').text(
-        'Du har Opera, versjon ' + navigator.userAgent.slice(119, 120)
+      console.log("User has Opera installed.");
+      $("#txtUt").text(
+        "Du har Opera, versjon " + navigator.userAgent.slice(119, 120)
       );
-    } else if (navigator.userAgent.indexOf('Firefox') != -1) {
-      console.log('User has Firefox installed.');
-      $('#txtUt').text(
-        'Du har Firefox, versjon ' + navigator.userAgent.slice(74, 76)
+    } else if (navigator.userAgent.indexOf("Firefox") != -1) {
+      console.log("User has Firefox installed.");
+      $("#txtUt").text(
+        "Du har Firefox, versjon " + navigator.userAgent.slice(74, 76)
       );
-    } else if (navigator.userAgent.indexOf('Chrome') != -1) {
-      console.log('User has Chrome installed.');
-      $('#txtUt').text(
-        'Du har Google Chrome, versjon ' + navigator.userAgent.slice(88, 90)
-      );
-    } else if (navigator.userAgent.indexOf('Safari') != -1) {
-      console.log('User has Safari installed.');
-      $('#txtUt').text('Du har Safari, versjonummeret kan ikke hentes');
+    } else if (navigator.userAgent.indexOf("Chrome") != -1) {
+      if (/Macintosh/.test(navigator.userAgent)) {
+      } else {
+        console.log("User has Chrome installed.");
+        console.log(navigator.userAgent);
+        $("#txtUt").text(
+          "Du har Google Chrome, versjon " + navigator.userAgent.slice(88, 90)
+        );
+      }
+    } else if (navigator.userAgent.indexOf("Safari") != -1) {
+      console.log("User has Safari installed.");
+      $("#txtUt").text("Du har Safari, versjonummeret kan ikke hentes");
     } else {
-      console.log('User has a unknown browser installed');
-      $('#txtUt').text('Du har en ukjent nettleser innstallert');
+      console.log("User has a unknown browser installed");
+      $("#txtUt").text("Du har en ukjent nettleser innstallert");
     }
   });
 });
